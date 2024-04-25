4248e7b6eb7a4f7101961a9e665d460a574a0ee6
smorkl <smsalazar0319@gmail.com>
2024-04-25 12:31:03 -0500

    create

---
+++ shell.c
@@ -0,0 +1,46 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <string.h>
+
+int main()
+{
+    char *buffer = NULL;
+    size_t size_of_buffer = 0;
+    char *args[] = {NULL, NULL};
+    pid_t pid;
+
+    while (1)
+    {
+        printf("%s", "#cisfun$ ");
+        
+        if (getline(&buffer, &size_of_buffer, stdin) == EOF)
+            break;
+        if (buffer[0] == '\n')
+            continue;
+
+        
+        buffer[strcspn(buffer, "\n")] = '\0';
+
+        
+        args[0] = buffer;
+
+        pid = fork();
+        if (pid == -1) {
+            perror("fork");
+            exit(EXIT_FAILURE);
+        } else if (pid == 0) {
+            execve(args[0], args, NULL);
+            perror("./shell");
+            exit(EXIT_FAILURE);
+        } else {
+            int status;
+            waitpid(pid, &status, 0);
+        }
+    }
+
+    free(buffer);
+    return 0;
+}
