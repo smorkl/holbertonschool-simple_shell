861e1562148a240fa4db8ad6be9e704091efedda
smorkl <smsalazar0319@gmail.com>
2024-04-26 01:20:54 -0500

    modifique tokenize

--- tokenize.c
+++ tokenize.c
@@ -3,53 +3,52 @@
 #include <stdio.h>
 #include "shell.h"
 
-char** tokenize(char* buffer)
+/**
+ * tokenize - Tokenizes a string into an array of tokens.
+ * @buffer: The string to tokenize.
+ *
+ * Return: An array of tokens.
+ */
+char **tokenize(char *buffer)
 {
-    char** args;
-    char* buffer_copy = strdup(buffer);
-    char* token = NULL;
-    int i = 0;
-    int num_tokens = 1; 
-    char* ptr = buffer;
+	char **args;
+	char *buffer_copy = strdup(buffer);
+	char *token = NULL;
+	int i = 0, num_tokens = 1;
+	char *ptr = buffer;
 
-    while (*ptr) 
-    {
-        if (*ptr == ' ') 
-        {
-            num_tokens++;
-        }
-        ptr++;
-    }
+	while (*ptr)
+	{
+		if (*ptr == ' ')
+			num_tokens++;
+		ptr++;
+	}
 
-    args = (char**)malloc((num_tokens + 1) * sizeof(char*));
-    if (args == NULL) 
-    {
-        perror("malloc");
-        exit(EXIT_FAILURE);
-    }
+	args = malloc((num_tokens + 1) * sizeof(char *));
+	if (args == NULL)
+	{
+		perror("malloc");
+		exit(EXIT_FAILURE);
+	}
 
-    token = strtok(buffer_copy, " ");
-    if (token == NULL) {
-        args[i++] = strdup("");
-    } else {
-        args[i++] = strdup(token);
-    }
+	token = strtok(buffer_copy, " ");
+	if (token == NULL)
+		args[i++] = strdup("");
+	else
+		args[i++] = strdup(token);
 
-    while (token != NULL)
-    {
-        token = strtok(NULL, " ");
-        if (token != NULL) {
-            args[i++] = strdup(token);
-        }
-    }
+	while (token != NULL)
+	{
+		token = strtok(NULL, " ");
+		if (token != NULL)
+			args[i++] = strdup(token);
+	}
 
-    args[i] = NULL;
-    for (int j = 0; j < i; j++)
-    {
-        free(args[j]);
-    }
+	args[i] = NULL;
+	for (int j = 0; j < i; j++)
+		free(args[j]);
 
-    free(buffer_copy);
+	free(buffer_copy);
 
-    return args;
+	return args;
 }
