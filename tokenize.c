bb738e9f799cfdd553070be06fe1c27bb3dd20ec
smorkl <smsalazar0319@gmail.com>
2024-04-26 01:13:43 -0500

    modificar

--- tokenize.c
+++ tokenize.c
@@ -5,24 +5,51 @@
 
 char** tokenize(char* buffer)
 {
-    char** args = (char**)malloc(100 * sizeof(char*));
+    char** args;
+    char* buffer_copy = strdup(buffer);
     char* token = NULL;
     int i = 0;
-    
-    if (args == NULL)
+    int num_tokens = 1; 
+    char* ptr = buffer;
+
+    while (*ptr) 
+    {
+        if (*ptr == ' ') 
+        {
+            num_tokens++;
+        }
+        ptr++;
+    }
+
+    args = (char**)malloc((num_tokens + 1) * sizeof(char*));
+    if (args == NULL) 
     {
         perror("malloc");
         exit(EXIT_FAILURE);
     }
 
-    token = strtok(buffer, " ");
+    token = strtok(buffer_copy, " ");
+    if (token == NULL) {
+        args[i++] = strdup("");
+    } else {
+        args[i++] = strdup(token);
+    }
 
     while (token != NULL)
     {
-        args[i++] = token;
         token = strtok(NULL, " ");
+        if (token != NULL) {
+            args[i++] = strdup(token);
+        }
     }
 
     args[i] = NULL;
+    for (int j = 0; j < i; j++)
+    {
+        free(args[j]);
+    }
+
+    free(buffer_copy);
+
     return args;
 }
