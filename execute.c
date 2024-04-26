85ad1d9e5b7a890ee2dcbcf8fe4911e2b159e8c8
smorkl <smsalazar0319@gmail.com>
2024-04-26 09:38:04 -0500

    organizando betty

--- execute.c
+++ execute.c
@@ -6,28 +6,30 @@
  */
 void execute_command(char **args)
 {
-    pid_t pid = fork();
-    if (pid == -1)
-    {
-        perror("fork");
-        exit(EXIT_FAILURE);
-    }
-    else if (pid == 0)
-    {
-        if (args[0] == NULL)
-        {
-            exit(EXIT_SUCCESS);
-        }
-        else
-        {
-            execve(args[0], args, NULL);
-            perror("./shell");
-            exit(EXIT_FAILURE);
-        }
-    }
-    else
-    {
-        int status;
-        waitpid(pid, &status, 0);
-    }
+	pid_t pid = fork();
+
+	if (pid == -1)
+	{
+		perror("fork");
+		exit(EXIT_FAILURE);
+	}
+	else if (pid == 0)
+	{
+		if (args[0] == NULL)
+		{
+			exit(EXIT_SUCCESS);
+		}
+		else
+		{
+			execve(args[0], args, NULL);
+			perror("./shell");
+			exit(EXIT_FAILURE);
+		}
+	}
+	else
+	{
+		int status;
+
+		waitpid(pid, &status, 0);
+	}
 }
