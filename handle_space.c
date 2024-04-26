9ab57a9c4670ef0cb7894268dcb86051892c4ffd
smorkl <smsalazar0319@gmail.com>
2024-04-26 09:28:57 -0500

    organizando betty

--- handle_space.c
+++ handle_space.c
@@ -9,13 +9,15 @@
  */
 char **initialize_args()
 {
-    char **args = malloc(2 * sizeof(char *));
-    if (args == NULL)
-    {
-        perror("malloc");
-        exit(EXIT_FAILURE);
-    }
-    args[0] = NULL;
-    args[1] = NULL;
-    return args;
+	char **args = malloc(2 * sizeof(char *));
+
+	if (args == NULL)
+	{
+		perror("malloc");
+		exit(EXIT_FAILURE);
+	}
+	args[0] = NULL;
+	args[1] = NULL;
+
+	return (args);
 }
