c05af091771491bad39d9adfe54e402828c93151
ivanpachecos <ivanpachecos@yahoo.com>
2024-04-26 09:10:42 -0500

    add file change name

---
+++ handle_space.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "shell.h"
+
+/**
+ * initialize_args - Initializes an array of strings for command arguments.
+ *
+ * Return: An array of strings initialized with two NULL elements.
+ */
+char **initialize_args()
+{
+    char **args = malloc(2 * sizeof(char *));
+    if (args == NULL)
+    {
+        perror("malloc");
+        exit(EXIT_FAILURE);
+    }
+    args[0] = NULL;
+    args[1] = NULL;
+    return args;
+}
