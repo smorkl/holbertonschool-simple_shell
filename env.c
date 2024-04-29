bb95582af13aa239f8781191c868cca71907807c
smorkl <smsalazar0319@gmail.com>
2024-04-29 13:22:02 -0500

    modificar

--- env.c
+++ env.c
@@ -1,16 +1,20 @@
+#include <unistd.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include "shell.h"
+
+extern char **environ;
 
 /**
  * print_environment - Prints the current environment variables.
  */
-void print_environment(void)
+void print_environment()
 {
-	char **env = environ;
-
+    char **env = environ;
+    
 	while (*env != NULL)
 	{
-		printf("%s\n", *env);
-		env++;
-	}
+        printf("%s\n", *env);
+        env++;
+    }
 }
